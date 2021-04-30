f465e14fc6e7069f3d0e83a342c984254ec7e10a
leonardonichele <leonardonichelework@outlook.com>
2021-04-30 00:23:09 -0300

    site 2.0

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,79 +1,96 @@
+import * as React from 'react';
 import '../styles/landing.css';
-import fl from '../assets/logo_black.png';
-import flTwo from '../assets/fl-grey.png';
-import flblack from '../assets/flblack.png';
-import scrollImg from '../assets/down.webp';
-import whatsappImg from '../assets/whatsapp.png';
+import logoImg from '../assets/logo.png';
+import heroImg from '../assets/hero.png';
+import whatsImg from '../assets/whats.png';
+import designImg from '../assets/design.png';
+import reqImg from '../assets/requisitos.png';
+import devImg from '../assets/dev.png';
+import showImg from '../assets/show.png';
 
 function Landing() {
     return (
-        <div id="homepage">
+        <div className="content">
             <header>
-                <div className="navbar">
-                <a href="/">
-                    <img src={fl} alt="logo"></img>
-                </a>
+                <div className="topnav">
+                    <img src={logoImg} alt="flowslabs"/>
                 </div>
             </header>
-            <div className="main">
-                <img src={flblack} alt="gif-init" />
-                <br></br>
-                <a href="/jobs">Sobre nós</a>
-                <br></br>
-            </div>
-            <div className="markdown">
-                <h3>Informações</h3>
-                <div className="main-img">
-                    <img src={scrollImg} alt="scrollImg" />
+            <main>
+                <div className="hero">
+                    <img src={heroImg} alt="banner"/>
                 </div>
-            </div>
-            <a href="https://api.whatsapp.com/send?phone=5551998810795" className="float">
-                <i className="fa fa-plus my-float"><img src={whatsappImg} alt="whatsapp" /></i>
-            </a>
-            <div className="line-colored"></div>
-            <div className="information">
-                <section className="list">
-                    <div className="bg-one-user">
-                        <h1>Soluções inteligentes,<br></br>para todos os tipos de negócios.</h1>
-                        <h3>Seja você pessoa física ou jurídica.</h3>
-                    </div>
-                    <div className="line-colored"></div>
-                    <div className="bg-two-dev">
-                        <h1>Design e desenvolvimento web.</h1>
-                        <h3>Da arquitetura até seu desenvolvimento funcional.<br></br>Tudo de forma descomplicada.</h3>
-                    </div>
-                </section>
-            </div>
-            <div className="line-colored"></div>
-            <div className="contact-us">
-                <h1>Deseja saber mais? Entre em contato conosco.</h1>
-                <br></br>
-                <a href="mailto:flowslabswork@outlook.com">Contate-nos</a>
-                <br></br>
-                <br/>
+                <div className="about">
+                    <h1>FlowsLabs</h1>
+                    <h3>Quem somos nós?</h3>
+                    <p>Uma empresa que surgiu na vontade de inovar, desde as pequenas coisas até as grandes.<br></br>Criado por Leonardo da Silva Nichele.<br></br><br/>
+                    Desenvolvemos soluções digitais incluindo:<br></br>
+                    - Desenvolvimento Web.<br></br>
+                    - Design. (Web/Mobile/Banners).</p>
+                    <br/>
+                    <a href="https://api.whatsapp.com/send?phone=5551998810795"><img src={whatsImg} alt="whatsapp"/></a>
+                </div>
                 <hr/>
-                <br/>
-                <h3>Nossas realizações e métodos.</h3>
-                <a href="/jobs">Projetos</a>
-                <br/>
-            </div>
-            <div className="line-colored"></div>
-            <footer>
-                <div className="infobar">
-                    <a href="/">
-                        <img src={flTwo} alt="logo" />
-                    </a>
-                    <div className="line"></div>
-                    <div className="sct-routes">
-                        <section>
-                            <a href="/">Início</a>
-                            <br></br>
-                            <a href="mailto:flowslabswork@outlook.com">Trabalhe conosco</a>
-                        </section>
+                <div className="method">
+                    <h1>Nosso método de desenvolvimento</h1>
+                    <div className="row">
+                        <div className="column">
+                            <div className="card">
+                                <img src={designImg} alt="design"/>
+                                <h3>Design</h3>
+                                <br/>
+                                <p>Estudo visual com tendências de satisfazer o cliente e suas necessidades.</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={reqImg} alt="requisitos"/>
+                                <h3>Requisitos</h3>
+                                <br/>
+                                <p>Definição de métricas a fins de garantir estabilidade e performance.</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={devImg} alt="developer"/>
+                                <h3>Desenvolver e testes</h3>
+                                <br/>
+                                <p>Hora do código + verificações de qualidade.</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <img src={showImg} alt="apresentação"/>
+                                <h3>Apresentação de projeto</h3>
+                                <br/>
+                                <p>Exibição ao cliente de versão finalizada.</p>
+                            </div>
+                        </div>
                     </div>
-                    <h1>Copyright © 2021 FlowsLabs. Todos os direitos reservados.<br></br>
-                    CNPJ nº 35.004.327/0001-16 - Rio Grande do Sul / Brasil.</h1>
                 </div>
+                <div className="work">
+                    <h1>Realizações</h1>
+                    <div className="juan-case">
+                        <p>Juan<b> Amarante</b></p>
+                        <h2>Projeto: JFSA-Web</h2>
+                        <br/>
+                        <h3>Site profissional no setor de tecnologia e gestão.</h3>
+                        <br/>
+                        <br/>
+                        <a href="https://juanamarante.com">Acessar site</a>
+                    </div>
+                </div>
+                <div className="contact">
+                    <h1>Contate-me</h1>
+                    <p>Caso tenha interesse em estabelecer contato, clique abaixo.</p>
+                    <br/>
+                    <br/>
+                    <a href="mailto:flowslabswork@outlook.com">Enviar mensagem</a>
+                </div>
+            </main>
+            <footer>
+                <p>Copyright © 2021 FlowsLabs. Todos os direitos reservados.<br></br>
+                CNPJ nº 35.004.327/0001-16 - Rio Grande do Sul / Brasil.</p>
             </footer>
         </div>
     );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/landing';
-import Portfolio from './pages/portfolio';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
-                <Route path="/jobs" exact component={Portfolio} />
             </Switch>
         </BrowserRouter>
     );
